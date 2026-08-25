0545bc40292b508ba9d93134d1ac0459b35341ae
Jinzo <91829829+pernio@users.noreply.github.com>
2026-08-25 19:45:23 +0200

    Add website file structure

---
+++ website/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-4xl font-bold text-white">Hello React!</h1>
+    </main>
+  );
+}
+
+export default App;
